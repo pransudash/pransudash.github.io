ab72cdc6c01f6c65e8619b4c72767a033fdcff06
Pransu Dash <pransudash@gmail.com>
2015-12-24 18:45:49 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -72,7 +72,7 @@ particlesJS("particles_js", {
     "events": {
       "onhover": {
         "enable": true,
-        "mode": "bubble"
+        "mode": "grab"
       },
       "onclick": {
         "enable": true,
